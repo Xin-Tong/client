678c89b96ef57fa9f6057913563b50f2f7312cb0
Xin-Tong <tongxinhit@gmail.com>
2014-05-08 15:05:14 -0400

    add oauth info

---
+++ oauth.sh
@@ -0,0 +1,6 @@
+export consumerKey=565f553d72a282f2ab501ccd6ec925
+export consumerSecret=f5ff6950bd
+export token=4b9606385d8def9158b84482619485
+export tokenSecret=2a6eafba9c
+oauth_verifier=97282f7875
+
