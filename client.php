53caf0a4d50beed40f95c93eb77b596f6a26d58e
Yiyang Chang <chang256@purdue.edu>
2014-05-09 15:34:10 -0400

    updated Readme, cleaned client.php

--- client.php
+++ client.php
@@ -1,31 +1,29 @@
-#!/usr/bin/php
-
 <?php
+$ch = curl_init();
+$arguments = getopt('h:o:i:p:t:');
    
-   $ch = curl_init();
-   
-   $arguments = getopt('h:o:i:p:t:');
-   
-   if(isset($arguments['h']))
-       $host = $arguments['h']; 
-//   $method = 'get';
-    $url="http://$host/openphoto.php?";  
+if(isset($arguments['h']))
+    $host = $arguments['h']; 
+
+$url="http://$host/openphoto.php?";  
+
 if(isset($arguments['o'])){
-      $operation = $arguments['o'];
+    $operation = $arguments['o'];
     $url .= "operation=$operation&";
 }
-  if(isset($arguments['i'])){
-      $ID = $arguments['i'];
-      $url .= "ID=$ID&";
+
+if(isset($arguments['i'])){
+    $ID = $arguments['i'];
+    $url .= "ID=$ID&";
 }
-  if(isset($arguments['p'])){
-      $path = $arguments['p'];
-      $data = array('file' => "@$path"); // Yiyang
-      $url .= "path=$path&";
+if(isset($arguments['p'])){
+    $path = $arguments['p'];
+    $data = array('file' => "@$path");
+    $url .= "path=$path&";
 }
-  if(isset($arguments['t'])){
-      $tags = $arguments['t'];
-      $url .= "tags=$tags&";  
+if(isset($arguments['t'])){
+    $tags = $arguments['t'];
+    $url .= "tags=$tags&";  
 }
 $consumerKey=getenv('consumerKey');
 $consumerSecret = getenv('consumerSecret');
@@ -36,23 +34,18 @@ $url .= "consumerKey=$consumerKey&";
 $url .= "consumerSecret=$consumerSecret&";
 $url .= "token=$token&";
 $url .= "tokenSecret=$tokenSecret";
-echo $url . "\n";
+//echo $url . "\n";
 
-
-  curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $url);
 if(isset($arguments['p'])){
-  curl_setopt($ch, CURLOPT_POST, 1); // Yiyang
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data); //Yiyang
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 }
 
-  
-  
-  #curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
- 
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  $resp = curl_exec($ch);
-  curl_close($ch);
-  echo $resp;
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $resp = curl_exec($ch);
+    curl_close($ch);
+    echo $resp;
   
 ?>
 
